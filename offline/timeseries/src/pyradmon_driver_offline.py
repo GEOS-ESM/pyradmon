83c4a4c0ae294c1f38eb1abc7cce3bdf36b08b2a
cohen-seth <scohen983@gmail.com>
2025-05-23 17:35:57 -0400

    debug

--- offline/timeseries/src/pyradmon_driver_offline.py
+++ offline/timeseries/src/pyradmon_driver_offline.py
@@ -238,9 +238,10 @@ class PyRadmonBase:
         """
         print(f'----------- self ---- : {self}')
 
+
         try:
-            
-            subprocess.run(["./pyradmon_bin2txt_driver.csh", self.exprc]) #'test_config_yaml_path.yaml']) #exprc])
+            self.pyradmon_bin2txt_driver = os.path.join(self.pyradmon, 'offline/timeseries/src/pyradmon_bin2txt_driver.csh')
+            subprocess.run([self.pyradmon_bin2txt_driver, self.exprc]) #'test_config_yaml_path.yaml']) #exprc])
             #subprocess.run([os.path.join(self.pyradmon, 'pyradmon_bin2txt_driver.csh'), self.exprc]) #exprc])
         except Exception as e:
             error_message = f"Error: {e}"
@@ -258,9 +259,9 @@ class PyRadmonBase:
         """
 
         #os.environ['rcfile'] = self
-
         try:
-            subprocess.run(["./pyradmon_img_driver.csh", self.exprc]) # 'test_config_yaml_path.yaml'])
+            self.pyradmon_img_driver = os.path.join(self.pyradmon, 'offline/timeseries/src/pyradmon_img_driver.csh')
+            subprocess.run([self.pyradmon_img_driver, self.exprc]) # 'test_config_yaml_path.yaml'])
             #subprocess.run([os.path.join(self.pyradmon, 'pyradmon_img_driver.csh'), self.exprc]) #exprc])
         except Exception as e:
             error_message = f"Error: {e}"
