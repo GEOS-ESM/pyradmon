a151ad5fad629f0754aec14390e35b992f8509ce
cohen-seth <scohen983@gmail.com>
2025-10-16 19:51:25 -0400

    README overhaul. Moved all unused files to archive folders. Timeseries yaml restructuring. ignore files.

---
+++ load_radmon_config.tcsh
@@ -0,0 +1,7 @@
+#!/bin/tcsh
+
+source .venv/bin/activate.csh
+setenv pyradmon $PWD
+setenv pyradmon_spatial_src $pyradmon/offline/spatial/src
+setenv pyradmon_timeseries_src $pyradmon/offline/timeseries/src
+# setenv run_dir $PWD/run_dir
--- load_radmon_config.zsh
+++ load_radmon_config.zsh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
 source .venv/bin/activate
 export pyradmon=$PWD
 export pyradmon_spatial_src=$pyradmon/offline/spatial/src
 export pyradmon_timeseries_src=$pyradmon/offline/timeseries/src
-
 # export run_dir=$PWD/run_dir
